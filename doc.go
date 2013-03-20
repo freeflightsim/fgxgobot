1f939f84f5a90af4369a98b26515a64ce9e681af
Peter Morgan <pete@daffodil.uk.com>
2013-03-20 13:45:34 +0000

    main doc

--- doc.go
+++ doc.go
@@ -18,3 +18,9 @@ An experimental FG Multi Player Network Bot written in Go (http://golang.org)
 
 */
 package main
+
+import(
+	"github.com/fgx/fgxgobot/crossfeed"
+	"github.com/fgx/fgxgobot/flights"
+	"github.com/fgx/fgxgobot/www"
+)
