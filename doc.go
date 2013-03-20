bbb023cfff03bbc3974419e2913a442cdb394ef8
Peter Morgan <pete@daffodil.uk.com>
2013-03-20 13:52:09 +0000

    main doc

--- doc.go
+++ doc.go
@@ -2,19 +2,11 @@
 // Use of this source code is governed by the GPL Licence
 // license that can be found in the LICENSE file.
 
-
-
 /*
-=====================
 FGx Go Bot ! 
-An experimental FG Multi Player Network Bot written in Go (http://golang.org)
-=====================
 
-/crossfeed/ 
-	Remote ajax client for data from "crossfeed" (ie UDP to Ajax)
-	http://crossfeed.fgx.ch/data << returns all data of flights on mpnet
-	
-	
+An experimental FG Multi Player Network Bot written in Go (http://golang.org)
+
 
 */
 package main
