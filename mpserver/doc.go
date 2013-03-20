825cc7a09d388a517f1985f9457836bf4df8b1c0
Peter Morgan <pete@daffodil.uk.com>
2013-03-20 14:10:54 +0000

    another doc approach

--- mpserver/doc.go
+++ mpserver/doc.go
@@ -3,10 +3,21 @@ Package mpserver - The FlightGear Multiplayer Servers
 
 About fgms
 
-The Mp Servers are instances of the FlightGear Multiplayer Server (fgms)
+The Mp Servers are instances of the "FlightGear Multiplayer Server" (fgms)
+	Newbie: http://wiki.flightgear.org/Howto:Multiplayer
+	Info:   http://wiki.flightgear.org/FlightGear_Multiplayer_Server
+	Code:   http://gitorious.org/fgms/fgms-0-x/trees/master
 
 Pilots flying in the sim connect to a running instance on a server and speak
 to each other with udp packets
+
+
+//= The MultiPlayers servers are instances of
+// the "FlightGear Multiplayer Server" - fgms
+//
+
+//
+// These Servers are discovered from DNS by looking up mpserver01.flightgear.org > n
 
 
 */
--- mpserver/mpserver.go
+++ mpserver/mpserver.go
@@ -6,16 +6,6 @@ import (
 	//"time"
 )
 
-//= The MultiPlayers servers are instances of
-// the "FlightGear Multiplayer Server" - fgms
-//
-// Newbie: http://wiki.flightgear.org/Howto:Multiplayer
-// Info:   http://wiki.flightgear.org/FlightGear_Multiplayer_Server
-// Code:   http://gitorious.org/fgms/fgms-0-x/trees/master
-//
-// These Servers are discovered from DNS by looking up mpserver01.flightgear.org > n
-
-
 
 
 
