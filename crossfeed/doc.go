86ce7dae560e104322113c309918ec5ed499318b
Peter Morgan <pete@daffodil.uk.com>
2013-03-20 14:25:05 +0000

    new docs method

--- crossfeed/doc.go
+++ crossfeed/doc.go
@@ -1,12 +1,15 @@
 /*
 Package crossfeed - Remote Client for crossfeed.fgx.ch
 
-About FGx Corssfeed
+About FGx Crossfeed
 
 The client aqquires remote flight data from Geoff's CrossFeed fgx-cf
 
 The crossfeed is a listener to the USP network and converts 
-a snapshot to json serves via ajax requests (see ../mpserver)
+a snapshot to json serves via ajax requests 
+
+(see ../mpserver)
+see github.com/fgx/fgxgobot/mpserver
 
 	Feed:     http://crossfeed.fgx.ch/data
 	Project:  http://fgx.ch/projects/fgx-cf
